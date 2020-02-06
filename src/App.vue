60cc9c0a8d2016d5367b1caeafa4874472eae186
Josh Cohmor <atelier.josh@gmail.com>
2020-02-06 23:30:56 +0900

    initialize

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <EnvironmentVariable />
+  </div>
+</template>
+
+<script>
+import EnvironmentVariable from './components/EnvironmentVariable.vue'
+
+export default {
+  name: 'app',
+  components: {
+    EnvironmentVariable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EnvironmentVariable.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="environment-variable">
+    <h1>{{ environment_variable }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EnvironmentVariable",
+  data: function() {
+    return { 
+      environment_variable: "" 
+    };
+  },
+  created() {
+    this.environment_variable = process.env.VUE_APP_API_URL;
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
